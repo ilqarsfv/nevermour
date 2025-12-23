b819dd9a5b2a81c94559f3ec5d9c066ad4d84b5b
Ilqar <ilqar.sofiyev.95@gmail.com>
2025-12-23 11:22:06 +0400

    v1

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+new Swiper(".bannerSwiper", {
+  speed: 1500,
+  parallax: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".custom-swiper-button-next",
+    prevEl: ".custom-swiper-button-prev",
+  },
+});
+
+new Swiper(".productSwiper", {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".custom-swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 1200,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+  },
+});
+
+$(".faq_card").on("click", function () {
+  $(this).find(".faq_btn").toggleClass("-rotate-135");
+  $(this).find(".faq_answer").slideToggle("slow");
+});
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 100) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
